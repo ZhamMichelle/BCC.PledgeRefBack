e63af450c28c5f487df2e02438833a5d2db5783f
KETTEBEZ <KETTEBEZ@PC000000024352>
2020-07-08 14:37:46 +0600

    forgot add

---
+++ BCC.PledgeRefBack/Migrations/20200708043535_First.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BCC.PledgeRefBack.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PledgeRefs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CityCodeKATO = table.Column<int>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    SectorCode = table.Column<string>(nullable: true),
+                    Sector = table.Column<int>(nullable: true),
+                    RelativityLocation = table.Column<string>(nullable: true),
+                    SectorDescription = table.Column<string>(nullable: true),
+                    TypeEstateCode = table.Column<string>(nullable: true),
+                    TypeEstateByRef = table.Column<string>(nullable: true),
+                    TypeEstate = table.Column<string>(nullable: true),
+                    ApartmentLayoutCode = table.Column<string>(nullable: true),
+                    ApartmentLayout = table.Column<string>(nullable: true),
+                    WallMaterialCode = table.Column<int>(nullable: true),
+                    WallMaterial = table.Column<string>(nullable: true),
+                    DetailAreaCode = table.Column<string>(nullable: true),
+                    DetailArea = table.Column<string>(nullable: true),
+                    MinCostPerSQM = table.Column<int>(nullable: true),
+                    MaxCostPerSQM = table.Column<int>(nullable: true),
+                    Corridor = table.Column<decimal>(nullable: false),
+                    MinCostWithBargain = table.Column<int>(nullable: true),
+                    MaxCostWithBargain = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PledgeRefs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PledgeRefs");
+        }
+    }
+}
---
+++ BCC.PledgeRefBack/Migrations/20200708075557_addDateFields.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BCC.PledgeRefBack.Migrations
+{
+    public partial class addDateFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BeginDate",
+                table: "PledgeRefs",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "PledgeRefs",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BeginDate",
+                table: "PledgeRefs");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "PledgeRefs");
+        }
+    }
+}

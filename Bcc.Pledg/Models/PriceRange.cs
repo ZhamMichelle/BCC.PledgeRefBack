9d0ac5a00ecc7461b1c8e0b81f27ea0cee0173e8
KETTEBEZ <KETTEBEZ@PC000000024352>
2020-11-05 10:38:45 +0600

    pricerange

---
+++ Bcc.Pledg/Models/PriceRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bcc.Pledg.Models
+{
+    public class PriceRange
+    {
+        public string CityCodeKATO { get; set; }
+        public int Sector { get; set; }
+        public string TypeEstateCode { get; set; }
+        public string? ApartmentLayoutCode { get; set; }
+        public int? WallMaterialCode { get; set; }
+        public string? DetailAreaCode { get; set; }
+    }
+}

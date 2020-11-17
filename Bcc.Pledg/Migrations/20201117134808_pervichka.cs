ac5ea71ca8b798fcb360e660fa55776d120022f9
shontykov <shontykov.kz@gmail.com>
2020-11-17 19:49:14 +0600

    pervichka ef migrations

---
+++ Bcc.Pledg/Migrations/20201117134808_pervichka.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bcc.Pledg.Migrations
+{
+    public partial class pervichka : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
